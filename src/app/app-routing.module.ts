b33b8ddb3c60d84577b31f77f380e8329306067e
Matheus Dornelles <matheus.dorne@gmail.com>
2024-03-31 16:18:21 -0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [  
+{
+  path: '', 
+  redirectTo: 'listarPensamento', // Tela inicial vai redirecionar para a listagem de pensamentos 
+  pathMatch: 'full' // A rota deve ser exatamente igual a definida em path 
+  //full = a rota deve ser exatamente igual a definida em path 
+  //prefix = a rota deve começar com o valor definido em path
+},
+{
+  path: 'criarPensamento', 
+  component: CriarPensamentoComponent 
+},
+{
+  path: 'listarPensamento', 
+  component: ListarPensamentoComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
